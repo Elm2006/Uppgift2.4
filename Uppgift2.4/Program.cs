210efd2d9e3b960b04dd773b2aa95b2caebb1bec
isel06002 <isel06002@E-PW0234HX>
2023-09-15 12:49:49 +0200

    Add project files.

---
+++ Uppgift2.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift_2.__4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv lönen för en av personerna...");
+        int lön1= int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv lönen för den andra personen...");
+            int lön2= int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv lönen för den tredje personen...");
+            int lön3 = int.Parse(Console.ReadLine());
+            int mid = (lön1 + lön2 + lön3) / 3;
+            Console.WriteLine("Medellönen av de tre personerna är " + mid + " kr");
+            Console.ReadKey();
+        }
+    }
+}
